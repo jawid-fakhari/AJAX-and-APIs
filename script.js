69b0d52a6336b7f119c9bc44bd9683376908a6e6
Jawid Fakhari <jawid_fakhari@live.it>
2023-10-06 12:21:49 +0200

    data.neighbour-added-callback-hell-concept

--- script.js
+++ script.js
@@ -4,36 +4,61 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//Render data
+const render = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>POP ${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>CUR ${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 //how to make a new API request
-const getCountryData = function (country) {
+const getCountryDataAndNeighbour = function (country) {
+  //AJAX call countery 1
   const request = new XMLHttpRequest();
   request.open(
     'GET',
     `https://countries-api-836d.onrender.com/countries/name/${country}`
   );
   request.send();
+
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `
-  <article class="country">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-  <h3 class="country__name">${data.name}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>POP ${data.population}</p>
-  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-  <p class="country__row"><span>💰</span>CUR ${data.currencies[0].name}</p>
-  </div>
-  </article>
-  `;
+    // render requested country 1
+    render(data);
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    //Get neighbour country 2
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) return;
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      'GET',
+      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+    );
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      render(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('japan');
-getCountryData('italy');
+getCountryDataAndNeighbour('italy');
